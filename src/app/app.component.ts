f16ddcd986ef412a6467fb3bc42cb156e769a4ab
evscott <eliotvscott@gmail.com>
2019-01-20 15:38:47 -0400

    Indentation fixed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,19 +13,20 @@ export class AppComponent {
   constructor(public http: HttpClient) {}
 
   private onKey(event: any) {
-    this.input = event.target.value;
+      this.input = event.target.value;
   }
 
   private async post() {
 
     //Input should be == 'SELECT * FROM Persons'
       if(this.input !== 'SELECT * FROM Persons') return;
-    const response = this.http.post('http://localhost:4201/users', {query: this.input}).subscribe(res => {
-      console.log('Response', res);
-      this.message = res.valueOf();
-    });
 
-    console.log(this.message);
-    this.input = '';
+      const response = this.http.post('http://localhost:4201/users', {query: this.input}).subscribe(res => {
+        console.log('Response', res);
+        this.message = res.valueOf();
+      });
+
+      console.log(this.message);
+      this.input = '';
   }
 }
