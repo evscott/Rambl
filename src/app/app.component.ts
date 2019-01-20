b7ab0d0607a9507b6589257d875283da394a78a5
evscott <eliotvscott@gmail.com>
2019-01-20 14:50:35 -0400

    Basic query.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,12 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  messages = this.http.get<any[]>('http://localhost:4201');
+  messages = this.http.get<any[]>('http://localhost:4201/users');
 
   constructor(public http: HttpClient) {}
 
   post() {
     const response = this.http.post('http://localhost:4201/users', {query: 'SELECT * FROM Persons'}).subscribe(next => console.log(next));
+    console.log(response);
   }
 }
