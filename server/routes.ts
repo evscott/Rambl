9682b864ba181fc8828ba4cf858cc397abc4fd98
evscott <eliotvscott@gmail.com>
2019-01-20 15:00:25 -0400

    Test DB query.

--- server/routes.ts
+++ server/routes.ts
@@ -3,17 +3,18 @@ const app = express.Router();
 const db = require('./Config.ts').mysql_pool;
 
 db.getConnection((err, connection) => {
-    if (err) {
-        console.error('Database connection failed: ' + err.stack);
-    }
-
+    if (err) console.error('Database connection failed: ' + err.stack);
     console.log('Connected to database.');
+
+    app.post('/users', (req, res) => {
+        db.query(req.body.query, function (err, result, fields) {
+            if (err) throw err;
+            console.log(result);
+            res.send({body: result})
+        });
+    });
 
     connection.release();
 });
-
-// Test get & post
-app.get('/users', (req, res) => res.send([]));
-app.post('/users', (req, res) => res.send({body: req.body}));
 
 export { app as routes };
