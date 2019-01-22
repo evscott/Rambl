baff2aeff53074792f4cc1d1f00ab58649850d78
evscott <eliotvscott@gmail.com>
2019-01-22 11:00:53 -0400

    Dist fix.

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static('./dist/your_app_name'));
 
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname,'/dist/your_app_name/index.html'));
+    res.sendFile(path.join(__dirname,'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
