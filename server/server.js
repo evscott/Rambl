8be57d305c889142d80bcda5d92921f37f9c5979
evscott <eliotvscott@gmail.com>
2019-01-22 11:35:21 -0400

    Skeleton of back-end server with examples of functionality.

--- server/server.js
+++ server/server.js
@@ -1,21 +1,31 @@
 const express = require('express');
 const http = require('http')
 const path = require('path');
-//import { routing } from './routing';
+const mysql = require('mysql');
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '../dist')));
 
-//app.use(routing);
-
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '../dist/index.html'));
 });
-
 
 const port = process.env.PORT || 3000;
 app.set('port', port);
 
 const server = http.createServer(app);
 server.listen(port, () => console.log('Listening on port ' + port));
+
+const con = mysql.createConnection({
+    host     : process.env.RDS_HOSTNAME || 'trippydatabase.cnioslqsy5gc.us-west-2.rds.amazonaws.com',
+    user     : process.env.RDS_USERNAME || 'escott07',
+    password : process.env.RDS_PASSWORD || 'password12yu',
+    port     : process.env.RDS_PORT || '3306',
+    database : process.env.RDS_DATABASE || 'trippydatabase'
+});
+
+con.connect(function(err) {
+    if (err) console.error('Database connection failed: ' + err.stack);
+    console.log('Connected to database.');
+});
