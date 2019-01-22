892d9953b4d214fae6388f96a6da0212488791a8
evscott <eliotvscott@gmail.com>
2019-01-22 12:06:42 -0400

    HEROKU

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,6 @@ export class AppComponent {
   constructor(public http: HttpClient) {}
 
   post() {
-    const response = this.http.post('http://localhost:4201', {username: 'Eliot', password: 'Scott'}).subscribe(next => console.log(next));
+    const response = this.http.post('https://trippy123.herokuapp.com/', {username: 'Eliot', password: 'Scott'}).subscribe(next => console.log(next));
   }
 }
