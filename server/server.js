fdd1906faf5dca5973a88c2acdf2cc4119477466
evscott <eliotvscott@gmail.com>
2019-01-22 11:14:06 -0400

    Server modularization.

--- server/server.js
+++ server/server.js
@@ -7,8 +7,10 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '../dist/index.html'));
 });
+
+
 
 const port = process.env.PORT || 3000;
 app.set('port', port);
