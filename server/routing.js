dba4f836d23b1c1e0bfdcdbba9daaf0c1239cec5
evscott <eliotvscott@gmail.com>
2019-01-22 11:22:52 -0400

    More server stuff.

---
+++ server/routing.js
@@ -0,0 +1,20 @@
+import * as express from 'express';
+const app = express.Router();
+const db = require('./Config.js').mysql_pool;
+
+db.getConnection((err, connection) => {
+    if (err) console.error('Database connection failed: ' + err.stack);
+    console.log('Connected to database.');
+
+    app.post('/users', (req, res) => {
+        db.query(req.body.query, function (err, result, fields) {
+            if (err) throw err;
+            console.log(result);
+            res.send({body: result})
+        });
+    });
+
+    connection.release();
+});
+
+export { app as routes };
--- server/server.js
+++ server/server.js
@@ -1,17 +1,33 @@
 const express = require('express');
 const http = require('http')
 const path = require('path');
-
+import { routes } from './routing';
 const app = express();
 
 app.use(express.static(path.join(__dirname, '../dist')));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'OPTIONS, POST, PUT, GET, DELETE');
+
+    if('OPTIONS' == req.method) {
+        res.sendStatus(200);
+    } else {
+        console.log(`${req.ip} ${req.method} ${req.url}`);
+        next();
+    }
+});
+
+app.use(routes);
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '../dist/index.html'));
 });
 
+
 const port = process.env.PORT || 3000;
 app.set('port', port);
 
 const server = http.createServer(app);
-server.listen(port, () => console.log('running'));
+server.listen(port, () => console.log('Listening on port ' + port));
