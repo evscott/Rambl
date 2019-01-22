b7f95b6605bf1148f400d727f4b6ec61645f251d
evscott <eliotvscott@gmail.com>
2019-01-22 10:59:18 -0400

    Heroku deployment test.

--- server/routes.ts
+++
@@ -1,8 +0,0 @@
-import * as express from 'express';
-const app = express.Router();
-
-export { app as routes };
-
-app.get('/',(req, res) => res.send([{'message': 'Hello World'}]));
-app.get('/users', (req, res) => res.send([]));
-app.post('/users', (req, res) => res.send({body: req.body}));
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,10 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  messages = this.http.get<any[]>('http://localhost:4201');
 
   constructor(public http: HttpClient) {}
 
   post() {
-    const response = this.http.post('http://localhost:4201/users', {username: 'Eliot', password: 'Scott'}).subscribe(next => console.log(next));
+    //const response = this.http.post('http://localhost:4201/users', {username: 'Eliot', password: 'Scott'}).subscribe(next => console.log(next));
   }
 }
