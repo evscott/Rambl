51ee4446d8dea08f32610ea53ef0af5887191a04
evscott <eliotvscott@gmail.com>
2019-01-23 22:59:53 -0400

    Fix.

--- trippy-api/server.js
+++ trippy-api/server.js
@@ -8,8 +8,13 @@ app.use(express.static(path.join(__dirname, '../dist')));
 app.use(express.json());
 app.use(Configs.AccessControl);
 
-app.get('*', (req, res) => res.sendFile(path.join(__dirname + '../dist/index.html')));
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname + '/../dist/index.html'))
+);
+
 app.get('/api/allusers', API.allUsers);
 //app.get('/users', (req, res) => res.send([{'message':'test'}]));
 
-app.listen(process.env.PORT || 4201, () => { console.log('Listening on port 4201...'); });
+app.listen(process.env.PORT || 4201, () => {
+  console.log('Listening on port 4201...');
+});
