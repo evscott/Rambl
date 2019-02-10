b9fe8aeb587231bcb4e32000bb5b2a22aa912a34
evscott <eliotvscott@gmail.com>
2019-02-09 23:17:38 -0400

    Deployment.

--- server/handlers/authHandler.js
+++ server/handlers/authHandler.js
@@ -28,7 +28,7 @@ let signup = async (req, res) => {
         // Signup unsuccessful - username potentially already taken.
         res.json({
           success: false,
-          message: 'Sign up unsuccessful.',
+          message: 'Sign up unsuccessful. Username or password already taken.',
           token: null
         });
       }
@@ -42,4 +42,66 @@ let signup = async (req, res) => {
       token: null
     });
   }
+};
+
+// Query database handler with login request
+let login = async (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+  // Check if username and password exist, return success notification
+  try {
+    databaseHandler.login(username, password).then(success => {
+      if (username && password) {
+        if (success) {
+          // Retrieve json web token
+          let token = jwt.sign(
+            { username: username, password: password },
+            Config.privateKey,
+            Config.signOptions
+          );
+
+          // Login success.
+          res.json({
+            success: true,
+            message: 'Authentication successful!',
+            token: token
+          });
+        } else {
+          // Credential authentication failure.
+          res.json({
+            success: false,
+            message: 'Incorrect username or password.',
+            token: null
+          });
+        }
+      } else {
+        // Ambiguous authentication failure.
+        res.json({
+          success: false,
+          message: 'Authentication failed! Please check the request.',
+          token: null
+        });
+      }
+    });
+  } catch (err) {
+    // Ambiguous failure - potentially fatal error.
+    console.err(err);
+    res.json({
+      success: false,
+      message: 'Authentication unsuccessful. This may be network related.'
+    });
+  }
+};
+
+let index = (req, res) => {
+  res.json({
+    success: true,
+    message: 'Index page'
+  });
+};
+
+module.exports = {
+  login: login,
+  signup: signup,
+  index: index
 };
