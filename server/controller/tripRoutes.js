b35f63df43c2fa28a6cdeaea1cd61b3fc1d6b03f
evscott <eliotvscott@gmail.com>
2019-02-18 11:30:12 -0400

    Removed trip routing params.

--- server/controller/tripRoutes.js
+++ server/controller/tripRoutes.js
@@ -6,7 +6,7 @@ const tripHandler = require('../handlers/tripHandler');
 // All the routes for trips
 router.get('/get', middleware.checkToken, tripHandler.getTrips);
 router.post('/add', middleware.checkToken, tripHandler.addTrip);
-router.put('/update/:tid', middleware.checkToken, tripHandler.updateTrip);
-router.delete('/delete/:tid', middleware.checkToken, tripHandler.deleteTrip);
+router.put('/update', middleware.checkToken, tripHandler.updateTrip);
+router.delete('/delete', middleware.checkToken, tripHandler.deleteTrip);
 
 module.exports = router;
