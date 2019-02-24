1a6abfe0ec5eb2d08b8150e8261ad7b5f4704ebd
Graeme Zinck <graeme.zinck@gmail.com>
2019-02-24 19:20:19 -0400

    Added a few basic background elements for the front end.

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,10 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
 import './App.css';
-import Root from "../containers/Root";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            [In Development]
-          </p>
-          <Root>document.getElementById("root")</Root>
-        </header>
+      <div className="App container">
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
 
 
 // If you want your app to work offline and load faster, you can change
