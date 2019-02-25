7cbfea2be5dfd937ea17cfc60c9e3b1ceb6030e1
Graeme Zinck <graeme.zinck@gmail.com>
2019-02-24 20:15:57 -0400

    Added the store to the Provider in index.js.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import Reducer from './redux/reducers/authReducers';
+
+const store = createStore(Reducer);
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
 
