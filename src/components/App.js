2514532129651a52ae1d5d14824b9c75aee2ec8c
Graeme Zinck <graeme.zinck@gmail.com>
2019-02-24 20:08:37 -0400

    Added the beginning of a router.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Routes from '../Routes';
 
 class App extends Component {
   render() {
     return (
       <div className="App container">
+        <Routes />
       </div>
     );
   }
