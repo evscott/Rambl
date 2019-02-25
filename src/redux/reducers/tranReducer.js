6f7e846e7e0d067fcf3b7434c62966110f313799
evscott <eliotvscott@gmail.com>
2019-02-25 09:50:41 -0400

    Updated action import notation.

--- src/redux/reducers/tranReducer.js
+++ src/redux/reducers/tranReducer.js
@@ -1,20 +1,4 @@
-import {
-  GET_TRANS_REQUEST,
-  GET_TRANS_FAILURE,
-  GET_TRANS_SUCCESS,
-  ADD_TRAN_REQUEST,
-  ADD_TRAN_FAILURE,
-  ADD_TRAN_SUCCESS,
-  UPDATE_TRAN_REQUEST,
-  UPDATE_TRAN_FAILURE,
-  UPDATE_TRAN_SUCCESS,
-  DELETE_TRAN_REQUEST,
-  DELETE_TRAN_FAILURE,
-  DELETE_TRAN_SUCCESS,
-  GET_TRAN_INFO_REQUEST,
-  GET_TRAN_INFO_FAILURE,
-  GET_TRAN_INFO_SUCCESS
-} from '../actions/tranActions';
+import * as TranActions from '../actions/tranActions';
 
 const initialState = {
   lastUpdated: null,
@@ -25,21 +9,21 @@ const initialState = {
 
 export function tranReducer(state = initialState, action) {
   switch (action.type) {
-    case GET_TRANS_REQUEST:
+    case TranActions.GET_TRANS_REQUEST:
       return {
         ...state,
         lastUpdated: action.lastUpdated,
         isFetching: action.isFetching,
         isSynced: action.isSynced
       };
-    case GET_TRANS_FAILURE:
+    case TranActions.GET_TRANS_FAILURE:
       return {
         ...state,
         lastUpdated: action.lastUpdated,
         isFetching: action.isFetching,
         isSynced: action.isSynced
       };
-    case GET_TRANS_SUCCESS:
+    case TranActions.GET_TRANS_SUCCESS:
       return {
         ...state,
         lastUpdated: action.lastUpdated,
@@ -47,63 +31,63 @@ export function tranReducer(state = initialState, action) {
         isSynced: action.isSynced,
         trans: action.trans
       };
-    case ADD_TRAN_REQUEST:
+    case TranActions.ADD_TRAN_REQUEST:
       return {
         ...state,
         lastUpdated: action.lastUpdated,
         isFetching: action.isFetching,
         isSynced: action.isSynced
       };
-    case ADD_TRAN_FAILURE:
+    case TranActions.ADD_TRAN_FAILURE:
       return {
         ...state,
         lastUpdated: action.lastUpdated,
         isFetching: action.isFetching,
         isSynced: action.isSynced
       };
-    case ADD_TRAN_SUCCESS:
+    case TranActions.ADD_TRAN_SUCCESS:
       return {
         ...state,
         lastUpdated: action.lastUpdated,
         isFetching: action.isFetching,
         isSynced: action.isSynced
       };
-    case UPDATE_TRAN_REQUEST:
+    case TranActions.UPDATE_TRAN_REQUEST:
       return {
         ...state,
         lastUpdated: action.lastUpdated,
         isFetching: action.isFetching,
         isSynced: action.isSynced
       };
-    case UPDATE_TRAN_FAILURE:
+    case TranActions.UPDATE_TRAN_FAILURE:
       return {
         ...state,
         lastUpdated: action.lastUpdated,
         isFetching: action.isFetching,
         isSynced: action.isSynced
       };
-    case UPDATE_TRAN_SUCCESS:
+    case TranActions.UPDATE_TRAN_SUCCESS:
       return {
         ...state,
         lastUpdated: action.lastUpdated,
         isFetching: action.isFetching,
         isSynced: action.isSynced
       };
-    case DELETE_TRAN_REQUEST:
+    case TranActions.DELETE_TRAN_REQUEST:
       return {
         ...state,
         lastUpdated: action.lastUpdated,
         isFetching: action.isFetching,
         isSynced: action.isSynced
       };
-    case DELETE_TRAN_FAILURE:
+    case TranActions.DELETE_TRAN_FAILURE:
       return {
         ...state,
         lastUpdated: action.lastUpdated,
         isFetching: action.isFetching,
         isSynced: action.isSynced
       };
-    case DELETE_TRAN_SUCCESS:
+    case TranActions.DELETE_TRAN_SUCCESS:
       return {
         ...state,
         lastUpdated: action.lastUpdated,
@@ -111,21 +95,21 @@ export function tranReducer(state = initialState, action) {
         isSynced: action.isSynced,
         trans: action.trans
       };
-    case GET_TRAN_INFO_REQUEST:
+    case TranActions.GET_TRAN_INFO_REQUEST:
       return {
         ...state,
         lastUpdated: action.lastUpdated,
         isFetching: action.isFetching,
         isSynced: action.isSynced
       };
-    case GET_TRAN_INFO_FAILURE:
+    case TranActions.GET_TRAN_INFO_FAILURE:
       return {
         ...state,
         lastUpdated: action.lastUpdated,
         isFetching: action.isFetching,
         isSynced: action.isSynced
       };
-    case GET_TRAN_INFO_SUCCESS:
+    case TranActions.GET_TRAN_INFO_SUCCESS:
       return {
         ...state,
         lastUpdated: action.lastUpdated,
