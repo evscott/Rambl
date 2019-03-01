5c4b3e897e09cf65f4f30b236a2ffc28f695c8f4
evscott <eliotvscott@gmail.com>
2019-03-01 12:33:26 -0400

    Cleanup.

--- server/server.js
+++ server/server.js
@@ -21,9 +21,6 @@ app.use(bodyParser.json());
 
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname + '/../build')));
-app.get('/ping', function (req, res) {
-  return res.send('pong');
-});
 app.route('/*', function (req, res) {
   res.sendFile(path.join(__dirname + '/../build/index.html'));
 });
