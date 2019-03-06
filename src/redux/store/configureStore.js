bad0bc2e86041c9690f2e7be134f4971de644b6d
evscott <eliotvscott@gmail.com>
2019-03-06 10:44:24 -0400

    Redux state data persistence, using redux-persist library.

--- src/redux/store/configureStore.js
+++ src/redux/store/configureStore.js
@@ -2,12 +2,22 @@ import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import rootReducer from '../reducers/rootReducer';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web and AsyncStorage for react-native
 
+const persistConfig = {
+  key: 'root',
+  storage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 const loggerMiddleware = createLogger();
 
 export default function configureStore() {
-  return createStore(
-    rootReducer,
+  let store = createStore(
+    persistedReducer,
     applyMiddleware(thunkMiddleware, loggerMiddleware)
   );
+  let persistor = persistStore(store)
+  return { store, persistor }
 }
