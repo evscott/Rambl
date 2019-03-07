202792c6c79bfb10c27092875189128bfd34afeb
Graeme Zinck <graeme.zinck@gmail.com>
2019-03-06 20:20:23 -0400

    Quickfix for linting.

--- src/components/IconLibrary.js
+++ src/components/IconLibrary.js
@@ -1,5 +1,9 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas, faStar as faStar1, faStarHalfAlt } from '@fortawesome/free-solid-svg-icons';
+import {
+  fas,
+  faStar as faStar1,
+  faStarHalfAlt
+} from '@fortawesome/free-solid-svg-icons';
 import { far, faStar as faStar2 } from '@fortawesome/free-regular-svg-icons';
 
 export function createIconLibrary() {
