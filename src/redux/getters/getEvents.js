c66818ea24098f914ab38ee5cc68ba37abdbfc16
morgankbender <mkbender@mta.ca>
2019-03-26 21:54:18 -0300

    Sort Events by priority

--- src/redux/getters/getEvents.js
+++ src/redux/getters/getEvents.js
@@ -117,3 +117,48 @@ export function getActiveEvents(state, tripId) {
 
   return { current: currEvents, upcoming: upcomingEvents };
 }
+
+/**
+ *  This gets all of the events that are flagged as high
+ *  priority (priority 2)
+ * @param state the state of the Redux store
+ * @param tripId the ID of the trip to check events for
+ * @returns {{highPriority: Array, medPriority: Array, lowPriority: Array}}
+ * arrays of all high priority, medium priority, and low priority events (respectively)
+ */
+export function prioritizedEvents(state, tripId) {
+  let hpEvents = [];
+  let mpEvents = [];
+  let lpEvents = [];
+
+  let plans = state.plans.plans[tripId];
+  let accoms = state.accoms.accoms[tripId];
+  let trans = state.trans.trans[tripId];
+
+  // Get all the values from the dictionaries (just get the raw event objects
+  // without worrying about their event ids). It only gets them if the section
+  // is not null.
+  let events = [];
+  if (plans != null) events = [...Object.values(plans)];
+  if (accoms != null) events = [...events, ...Object.values(accoms)];
+  if (trans != null) events = [...events, ...Object.values(trans)];
+
+  events.forEach((event) => {
+    if (event.priority === 2) {
+      hpEvents.push({ ...event });
+    } else if (event.priority === 1) {
+      mpEvents.push({ ...event });
+    } else {
+      lpEvents.push({ ...event });
+    }
+  });
+
+  sortEvents(hpEvents);
+  sortEvents(mpEvents);
+  sortEvents(lpEvents);
+  return {
+    highPriority: hpEvents,
+    medPriority: mpEvents,
+    lowPriority: lpEvents
+  };
+}
