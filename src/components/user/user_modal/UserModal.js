986a0f6685b8b4f44b78eecdf6211a34da148fd0
evscott <eliotvscott@gmail.com>
2019-03-28 14:59:41 -0300

    Added commenting.

--- src/components/user/user_modal/UserModal.js
+++ src/components/user/user_modal/UserModal.js
@@ -3,6 +3,12 @@ import { Modal } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import UserInfoFormContainer from '../user_info_form/UserInfoFormContainer';
 
+/**
+ * This component creates a user information modal. It accepts two properties:
+ * show and onHide. Show is a boolean value which determines whether the
+ * modal should be displayed, and onHide is a button which toggles the
+ * value of show.
+ */
 export class UserModal extends Component {
   render() {
     return (
