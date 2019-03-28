8ab49af04787fac91a9544078054bee14cdafea4
evscott <eliotvscott@gmail.com>
2019-03-28 14:48:16 -0300

    Added user update service to server and Redux store.

--- server/handlers/userHandler.js
+++ server/handlers/userHandler.js
@@ -2,6 +2,22 @@ const databaseHandler = require('./databaseHandler');
 const jwtDecoder = require('../shared/jwtDecoder');
 
 /**
+ * Updates the user info in the database.
+ * @param req the request
+ * @param res the response
+ * @returns {Promise<void>} the promise indicating success
+ */
+let updateUserInfo = async (req, res) => {
+  let token = req.headers['x-access-token'];
+  let email = jwtDecoder(token);
+  const params = [req.body.f_name, req.body.l_name, email];
+  const query = `UPDATE users
+                 SET f_name = ?, l_name = ?
+                 WHERE email = ?;`;
+  return databaseHandler.queryDatabase(res, query, params, 'Update user info');
+};
+
+/**
  * Gets the user info from the database and gives
  * it back to the user using the res object. Note that
  * the user info is found in an array within the "result"
@@ -20,5 +36,6 @@ let getInfo = async (req, res) => {
 };
 
 module.exports = {
+  updateUserInfo: updateUserInfo,
   getInfo: getInfo
 };
