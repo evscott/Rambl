bb341e3f361319e26d1263ebefa228c921fbd996
ibishop <isaiahjonbishop@gmail.com>
2019-03-29 22:24:13 -0300

    Merge branch '#86' of https://github.com/evscott/Rambl into #89
    Login Page, Issue with passing properties

---
+++ src/components/dashboard/jumbotron/CountdownTimer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+
+/**
+ *
+ */
+
+export default class CountdownTimer extends React.Component {
+
+    getTimeLeft(){
+        var start = this.props.currTripInfo.trip_start;
+        var curr = new Date();
+        var timeDiff = start.getTime() - curr.getTime();
+        var daysleft = Math.floor(timeDiff / (1000 * 3600 * 24));
+        var hoursleft  = Math.floor(timeDiff - daysleft*(24));
+        var minutesleft = Math.floor( timeDiff - daysleft*(24)*(60) - hoursleft*(60));
+        var secondsleft = Math.floor(timeDiff - daysleft*(24)*(60)*(60) - hoursleft*(60)*(60) - minutesleft*(60)) ;
+        return { days: daysleft , hours: hoursleft, minutes: minutesleft, seconds: secondsleft }
+    }
+
+    getTimeDiv(){
+        var time = this.getTimeLeft()
+        return (<div><p>{time.days Days}</p>
+                <p>{time.hours Hours}</p>
+                <p>{time.minutes} </p>
+                <p>{time.seconds} </p></div>)
+    }
+
+
+    render() {
+        return (
+            <div> {this.getTimeDiv} </div>
+        );
+    }
+}
