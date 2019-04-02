4fa821a8cbb9a3f3af2e554c4cc40e819ce15054
morgankbender <mkbender@mta.ca>
2019-04-01 21:30:52 -0300

    Bypass landing page if logged in

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './components/auth/landing/Landing';
+import LandingContainer from './components/auth/landing/LandingContainer';
 import LoginContainer from './components/auth/login/LoginContainer';
 import SignUpContainer from './components/auth/signup/SignUpContainer';
 import DashboardContainer from './components/dashboard/DashboardContainer';
@@ -9,7 +9,7 @@ import { TripView } from './components/trip_view/TripView';
 export default () => (
   <Router>
     <Switch>
-      <Route exact path="/" component={Home} />
+      <Route exact path="/" component={LandingContainer} />
       <Route exact path="/login" component={LoginContainer} />
       <Route exact path="/signup" component={SignUpContainer} />
       <Route exact path="/dashboard" component={DashboardContainer} />
