32e58bac409f90f59a0a99083973c03dc8a4b890
ibishop <isaiahjonbishop@gmail.com>
2019-04-01 23:04:11 -0300

    Merge remote-tracking branch 'remotes/origin/master' into #179
    # Conflicts:
    #	src/components/dashboard/jumbotron/OngoingJumbotron.js
    #	src/components/dashboard/jumbotron/OngoingJumbotronContainer.js
    #	src/components/dashboard/jumbotron/UpcomingJumbotron.js

--- src/components/dashboard/jumbotron/CountdownTimer.js
+++ src/components/dashboard/jumbotron/CountdownTimer.js
@@ -1,5 +1,5 @@
 import React, { Component }  from 'react';
-
+import "./countdown.css"
 
 /**
  * Displays Time untill upcoming trip
@@ -8,32 +8,42 @@ import React, { Component }  from 'react';
 export default class CountdownTimer extends Component {
 
     /**************************** Helper functions ****************************/
-    getTimeLeft(){
-        var start = this.props.currTripInfo.trip_start;
+    getTimeLeft() {
+
+        var start = this.props.currTripInfo.trip.trip_start;
         var curr = new Date();
         var timeDiff = start.getTime() - curr.getTime();
         var daysleft = Math.floor(timeDiff / (1000 * 3600 * 24));
-        var hoursleft  = Math.floor(timeDiff - daysleft*(24));
-        var minutesleft = Math.floor( timeDiff - daysleft*(24)*(60) - hoursleft*(60));
-        var secondsleft = Math.floor(timeDiff - daysleft*(24)*(60)*(60) - hoursleft*(60)*(60) - minutesleft*(60)) ;
-        return { days: daysleft , hours: hoursleft, minutes: minutesleft, seconds: secondsleft }
+        var hoursleft  = Math.floor(timeDiff/ (1000* 3600) - daysleft*(24));
+        var minutesleft = Math.floor( timeDiff/ (1000* 60) - daysleft*(24)*(60) - hoursleft*(60));
+        var secondsleft = Math.floor(timeDiff/ (1000) - daysleft*(24)*(60)*(60) - hoursleft*(60)*(60) - minutesleft*(60));
+        return { days: daysleft, hours: hoursleft, minutes: minutesleft, seconds: secondsleft };
     }
 
     /**
       * Creates all HTML for countdown timer
       */
-    getTimeDiv(){
-        var time = this.getTimeLeft()
-        return (<div><p>{time.days }Days</p>
-                <p>{time.hours} Hours</p>
-                <p>{time.minutes}</p>
-                <p>{time.seconds} </p></div>)
+    getTimeDiv() {
+        var time = this.getTimeLeft();
+        return (<div className= "flex-wrap-center">
+                    <div className= "countdown-elem">
+                        <p>{time.days } Days</p>
+                    </div>
+                    <div className= "countdown-elem">
+                        <p>{time.hours } Hours</p>
+                    </div>
+                    <div  className= "countdown-elem">
+                        <p>{time.minutes } Minutes</p>
+                    </div>
+                    <div  className= "countdown-elem">
+                        <p>{time.days } Seconds</p>
+                    </div>
+                </div>);
     }
-
     /**************************** Visual component ****************************/
     render() {
         return (
-            <div> {this.getTimeDiv} </div>
+            <div>{this.getTimeDiv()}</div>
         );
     }
 }
